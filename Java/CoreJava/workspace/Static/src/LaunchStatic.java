a7b540cb8524d08730eb84dea1c9725df4472219
sultanaNargis03 <nargissultana969@gmail.com>
2024-05-04 01:42:31 +0530

    "method hiding"

--- Java/CoreJava/workspace/Static/src/LaunchStatic.java
+++ Java/CoreJava/workspace/Static/src/LaunchStatic.java
@@ -2,7 +2,11 @@ class Parent12
 {
 	static void disp()
 	{
-		System.out.println("static disp()");
+		System.out.println("parent static disp()");
+	}
+	 void test()
+	{
+		System.out.println("parent test()");
 	}
 }
 
@@ -14,15 +18,29 @@ class Child12 extends Parent12
 	{
 		System.out.println("child static");
 	}
+   void test()
+	{
+		System.out.println("child test");
+	}
+  
 }
 public class LaunchStatic 
 {
 
 	public static void main(String[] args) 
 	{
-		Child12.disp();
+		//method hiding
+	    Parent12 p=new Child12();
+	    p.disp();
+	    
+		Child12.disp();// behaving like a specialized mehod
         Child12 c=new Child12();
-        c.disp();
+        //c.disp();
+        
+        //method overriding
+        p.test();
+        c.test();
+
 	}
 
 }
