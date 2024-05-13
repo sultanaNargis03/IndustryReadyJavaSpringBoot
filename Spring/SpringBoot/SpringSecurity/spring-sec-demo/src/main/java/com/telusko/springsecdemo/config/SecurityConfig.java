cb740e4ac0ebbdc1f71fc72a63a8550a42cce85f
sultanaNargis03 <nargissultana969@gmail.com>
2024-05-13 22:25:54 +0530

    "password encoder"

--- Spring/SpringBoot/SpringSecurity/spring-sec-demo/src/main/java/com/telusko/springsecdemo/config/SecurityConfig.java
+++ Spring/SpringBoot/SpringSecurity/spring-sec-demo/src/main/java/com/telusko/springsecdemo/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,7 +29,7 @@ public class SecurityConfig
 	{
 		DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 		
 		return provider;
 	}
